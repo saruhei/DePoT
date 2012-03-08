46a4e9a1c4b806d675d8aae7507c68e80ac7f34b
Kazunori Sakamoto <exkazuu@gmail.com>
2012-03-08 15:53:10 +0900

    Remove unused files.

--- src/test/java/jp/ac/waseda/cs/washi/automake/AssertFunction.java
+++
@@ -1,5 +0,0 @@
-package jp.ac.waseda.cs.washi.automake;
-
-public interface AssertFunction<Tpage> {
-	public void assertPage(Tpage page);
-}
--- src/test/java/jp/ac/waseda/cs/washi/automake/UnExpectAction.java
+++
@@ -1,6 +0,0 @@
-package jp.ac.waseda.cs.washi.automake;
-
-public interface UnExpectAction<Tpage, Tpage2> {
-	public <T> Tpage unExpectAct(Tpage2 page) throws ClassNotFoundException;
-
-}
